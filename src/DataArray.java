f986656cbf2e24e9bc8ac3b84ebfeafe45ba0fcb
mauldoto <maulanaly97@gmail.com>
2022-04-23 18:17:53 +0700

    first commit

---
+++ src/DataArray.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class DataArray {
+
+    public static void main(String[] args) {
+        String[] arrString = new String[3];
+        arrString[0] = "Kamen Rider Build";
+
+        System.out.println(arrString[0]);
+
+        // array initilizer
+        String[] stringArr = {
+                "build", "Saber", "exaid"
+        };
+
+        int[] intArr = new int[]{
+                1,2,3,4,5,6
+        };
+    }
+}
