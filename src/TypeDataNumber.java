5ae814d08095244b31c6bb3a2dc424ece595e949
mauldoto <maulanaly97@gmail.com>
2022-04-23 10:40:13 +0700

    first commit

---
+++ src/TypeDataNumber.java
@@ -0,0 +1,18 @@
+public class TypeDataNumber {
+    // integer number
+    byte iniByte = 100;
+    short iniShort = 1000;
+    int iniInt = 10000000;
+    long iniLong = 1000000000;
+
+    // floating number
+    float iniFloat = 1.2F;
+    double iniDouble = 23.3434;
+
+    // literals
+    int decimalInt = 35;
+    int hexaDecimal = 0xfff;
+    int binaryDecimal = 0b010101;
+
+    // underscore
+}
